3bcb092d8d7f94be80a58958e3b026b3d6bc861a
Ayanavbha Chatterjee <117476380+aYgCOO@users.noreply.github.com>
2025-11-27 18:35:51 +0530

    fix(server): add catch-all route to serve index.html

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
@@ -11,6 +12,11 @@ const MAX_MESSAGES_PER_CIRCLE = 50;
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
+
+// Add a catch-all route to serve index.html
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // In-memory data store for circles
 const circles = {};
